baeff9052a31ca624fba00aa969deb78426197b3
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-11-25 12:50:01 -0500

    added code to header.js and header.css

---
+++ src/components/header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navigation from "./Navigation";
+import { Container, Row, Col } from "react-bootstrap";
+import "../style/Header.css";
+
+// Header component
+function Header() {
+  return (
+    <>
+      <Container fluid className="bg-dark">
+        <Row className="justify-content-center text-center mb-0">
+          <Col className="pb-0">
+            <h1 className="text-white" style={{ fontSize: "70px" }}>
+
+              <Link
+                to="/Ricky-Sama/React-Portfolio/"
+                className="text-white text-decoration-none"
+                style={{ margin: "0" }}
+              >
+                Ricardo Scuotto
+              </Link>
+            
+            </h1>
+          </Col>
+        </Row>
+      </Container>
+      <Navigation />
+    </>
+  );
+}
+
+// Export Header
+export default Header;
